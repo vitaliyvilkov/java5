ac07a76818525f869399b86b12720b03093c3d89
Vitaliy V <vitaliyvilkov@gmail.com>
2024-06-07 23:06:02 +0400

    initial commit

--- src/main/java/org/example/java5/services/HolidayService.java
+++ src/main/java/org/example/java5/services/HolidayService.java
@@ -1,4 +1,5 @@
 package org.example.java5.services;
+
 public class HolidayService {
     public int calculate(int income, int expenses, int threshold) {
         int count = 0;
@@ -6,7 +7,7 @@ public class HolidayService {
         for (int month = 0; month < 12; month++) {
             if (money >= threshold) {
                 count++;
-                money = (money - expenses)/3;
+                money = (money - expenses) / 3;
             } else {
                 money = money + income - expenses;
             }
--- src/test/java/org/example/java5/services/HolidayServiceTest.java
+++ src/test/java/org/example/java5/services/HolidayServiceTest.java
@@ -5,17 +5,18 @@ import org.junit.jupiter.api.Test;
 
 public class HolidayServiceTest {
     @Test
-    public void testChanceToRest1(){
+    public void testChanceToRest1() {
         HolidayService service = new HolidayService();
         int expected = 3;
         int actual = service.calculate(10000, 3000, 20000);
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void testChanceToRest2(){
+    public void testChanceToRest2() {
         HolidayService service = new HolidayService();
         int expected = 2;
         int actual = service.calculate(100000, 60000, 150000);
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
 }
